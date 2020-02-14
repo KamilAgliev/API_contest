6c669ffa669cfabf4fca2411e100e807f5c1fc26
Almet-Agliev4 <k.agliev@mail.ru>
2020-02-14 16:41:17 +0300

    first commit

--- Api_solver.py
+++ Api_solver.py
@@ -11,26 +11,43 @@ SCREEN_SIZE = [500, 500]
 class Example(QWidget):
     def __init__(self):
         super().__init__()
-        self.longitude = sys.argv[1]
-        self.latitude = sys.argv[2]
-        self.spn = sys.argv[3:]
-        params = {
-            "spn": ",".join(self.spn),
+        self.longitude = "52.340178"
+        self.latitude = "54.887520"
+        self.z = "12"
+        self.params = {
+            "z": self.z,
             "ll": self.longitude + ',' + self.latitude,
             "l": "map",
             "apikey": "40d1649f-0493-4b70-98ba-98533de7710b"
         }
         geocoder_server = "http://static-maps.yandex.ru/1.x/"
-        response = requests.get(geocoder_server, params=params)
-        print(response.url)
-        self.map = "map.png"
+        response = requests.get(geocoder_server, params=self.params)
         with open("map.png", "wb") as file:
             file.write(response.content)
         self.image_label = QLabel(self)
         self.image_label.resize(500, 500)
-        self.image_label.move(0, 0)
         self.image_label.setPixmap(QPixmap("map.png"))
-        self.image_label.show()
+
+    def keyPressEvent(self, event):
+        if event.key() == 16777238:
+            self.z = str(min(19, int(self.z) + 1))
+        if event.key() == 16777239:
+            self.z = str(max(2, int(self.z) - 1))
+        self.params = {
+            "z": self.z,
+            "ll": self.longitude + ',' + self.latitude,
+            "l": "map",
+            "apikey": "40d1649f-0493-4b70-98ba-98533de7710b"
+        }
+        self.update_map()
+
+    def update_map(self):
+        geocoder_server = "http://static-maps.yandex.ru/1.x/"
+        response = requests.get(geocoder_server, params=self.params)
+        print(response.url)
+        with open("map.png", "wb") as file:
+            file.write(response.content)
+        self.image_label.setPixmap(QPixmap("map.png"))
 
 
 if __name__ == '__main__':
